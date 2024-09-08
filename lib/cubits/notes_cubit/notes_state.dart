9684c056a172d3bfd5fc388b84eb14d8213dc56b
mostafa <Mostafaajep@gmail.com.com>
2024-09-08 19:53:16 +0300

    create notes state

---
+++ lib/cubits/notes_cubit/notes_state.dart
@@ -0,0 +1,18 @@
+part of 'notes_cubit.dart';
+
+@immutable
+sealed class NotesCubitState {}
+
+final class NotesCubitInitial extends NotesCubitState {}
+
+final class NotesCubitLoading extends NotesCubitState {}
+
+final class NotesCubitSuccess extends NotesCubitState {
+  final List<NoteModel> notes;
+  NotesCubitSuccess({required this.notes});
+}
+
+final class NotesCubitFailure extends NotesCubitState {
+  final String errorMessage;
+  NotesCubitFailure({required this.errorMessage});
+}
