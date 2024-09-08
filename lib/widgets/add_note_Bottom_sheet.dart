e5051e14a39f8c203e0b27d1a0f5124b402fc4d3
mostafa <Mostafaajep@gmail.com.com>
2024-09-08 10:59:44 +0300

    take input and validate data

--- lib/widgets/add_note_Bottom_sheet.dart
+++ lib/widgets/add_note_Bottom_sheet.dart
@@ -9,13 +9,58 @@ class ShowNoteBottomSheet extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Padding(
       padding: EdgeInsets.symmetric(horizontal: 16, vertical: 30),
+      child: AddNoteForm(),
+    );
+  }
+}
+
+class AddNoteForm extends StatefulWidget {
+  const AddNoteForm({
+    super.key,
+  });
+
+  @override
+  State<AddNoteForm> createState() => _AddNoteFormState();
+}
+
+class _AddNoteFormState extends State<AddNoteForm> {
+  final GlobalKey<FormState> formKey = GlobalKey();
+  AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
+  //any var should be in stateful widget if its changes so we do not but final before it
+  //in stateless widget we but final.
+  String? title, subtitle;
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: formKey,
+      autovalidateMode: autovalidateMode,
       child: Column(
         children: [
-          CustomTextField(hint: 'Title'),
-          SizedBox(height: 16),
-          CustomTextField(hint: 'Content', maxLines: 5),
-          Spacer(),
-          CustomButton(),
+          CustomTextField(
+              hint: 'Title',
+              onSaved: (value) {
+                title = value;
+              }),
+          const SizedBox(height: 16),
+          CustomTextField(
+              hint: 'Content',
+              maxLines: 5,
+              onSaved: (value) {
+                subtitle = value;
+              }),
+          const Spacer(),
+          CustomButton(
+            onTap: () {
+              if (formKey.currentState!.validate()) {
+                formKey.currentState!.save();
+                Navigator.pop(context, {'title': title, 'subtitle': subtitle});
+              } else {
+                setState(() {
+                  autovalidateMode = AutovalidateMode.always;
+                });
+              }
+            },
+          ),
         ],
       ),
     );
--- lib/widgets/custom_button.dart
+++ lib/widgets/custom_button.dart
@@ -2,12 +2,12 @@ import 'package:flutter/material.dart';
 import '../constants.dart';
 
 class CustomButton extends StatelessWidget {
-  const CustomButton({super.key});
-
+  const CustomButton({super.key, this.onTap});
+  final Function()? onTap;
   @override
   Widget build(BuildContext context) {
     return InkWell(
-      onTap: () {},
+      onTap: onTap,
       child: Container(
         width: MediaQuery.of(context).size.width,
         height: 50,
